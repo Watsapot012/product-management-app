b8f0ae88c98ca26371defe0c60cc966113eacc03
Watsapot Sriployrung <>
2026-09-17 16:09:38 +0700

    initial project successfully

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config({path:"./.env"});
+const PORT=process.env.BACKEND_PORT
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res
+    .status(200)
+    .send("<h>Restful API for Product Management App</h>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on : http://localhost:${PORT}`);
+});
